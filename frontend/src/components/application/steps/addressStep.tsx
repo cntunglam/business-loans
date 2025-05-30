80382e17010ba8067afb5acb9d48d9b1e966ca50
cngvc <joevu.work@gmail.com>
2025-05-30 11:44:23 +0700

    combine address steps

--- frontend/src/components/application/steps/addressStep.tsx
+++ frontend/src/components/application/steps/addressStep.tsx
@@ -1,14 +1,23 @@
+import { Option } from "@mui/joy";
 import { ApplicationSteps } from "@roshi/backend/services/applicationSteps.service";
 import { ApplicationStepsEnum } from "@roshi/shared";
 import { forwardRef, useEffect, useImperativeHandle, useState } from "react";
 import { useVisitorContext } from "../../../context/visitorContext";
-import { TEST_IDS } from "../../../utils/testUtils";
+import { districts, provinces } from "../../../data/addresses";
 import { Flex } from "../../shared/flex";
 import { ApplicationStyledInput } from "../styled/applicationStyledInput";
+import { ApplicationStyledSelect } from '../styled/applicationStyledSelect';
+
+interface AddressValue {
+  province?: string;
+  district?: string;
+  detail: string;
+}
 
 export const AddressStep = forwardRef<{ getValue: () => unknown }>((_, ref) => {
   const { error, setError, visitor } = useVisitorContext();
-  const [value, setValue] = useState<string>("");
+  const [value, setValue] = useState<AddressValue>({ detail: "" });
+  const [filteredDistricts, setFilteredDistricts] = useState<{ name: string, districtId: string }[]>([]);
 
   useEffect(() => {
     if (!visitor) return;
@@ -16,32 +25,80 @@ export const AddressStep = forwardRef<{ getValue: () => unknown }>((_, ref) => {
       const stepData = ApplicationSteps[ApplicationStepsEnum.currentAddress].validation(
         visitor[ApplicationStepsEnum.currentAddress],
       );
-      setValue(stepData);
+      const address = JSON.parse(stepData)
+      if (address && Array.isArray(address) && address.length === 3) {
+        const districtId = districts.find(d => d.name === address[1])?.districtId;
+        const provinceId = provinces.find(p => p.name === address[2])?.provinceId;
+        setValue({
+          province: provinceId,
+          district: districtId,
+          detail: address[0]
+        });
+        const filtered = districts.filter(d => d.provinceId === provinceId);
+        setFilteredDistricts(filtered);
+      }
     } catch (error) {
-      // do nothing
+      setValue({ detail: "" });
     }
   }, [visitor]);
 
   useImperativeHandle(ref, () => ({
     getValue: () => {
-      if (!value) {
-        setError("Please enter your city");
+      if (!value.province || !value.district || !value.detail) {
+        setError("Vui lòng điền đầy đủ thông tin địa chỉ");
         return;
       }
-      return value;
+      const district = districts.find(d => d.districtId === value.district)?.name || ""
+      const province = provinces.find(p => p.provinceId === value.province)?.name || ""
+      const address = [value.detail, district, province]
+      return JSON.stringify(address);
     },
   }));
 
   return (
     <Flex y xc gap2 px={{ xs: 3, sm: 2, md: 0 }}>
+      <ApplicationStyledSelect
+        placeholder="Chọn tỉnh/thành phố"
+        value={value.province}
+        onChange={(_, val) => {
+          if (val === value.province) return;
+          setError("");
+          setValue(prev => ({ ...prev, province: val as string, district: undefined }))
+          const filtered = districts.filter(d => d.provinceId === val);
+          setFilteredDistricts(filtered);
+        }}
+      >
+        {provinces.map((option) => (
+          <Option key={option.provinceId} value={option.provinceId}>
+            {option.name}
+          </Option>
+        ))}
+      </ApplicationStyledSelect>
+
+      <ApplicationStyledSelect
+        placeholder="Chọn quận/huyện"
+        value={value.district}
+        disabled={!value.province}
+        onChange={(_, val) => {
+          setError("");
+          setValue(prev => ({ ...prev, district: val as string }))
+        }}
+      >
+        {filteredDistricts.map((option) => (
+          <Option key={option.districtId} value={option.districtId}>
+            {option.name}
+          </Option>
+        ))}
+      </ApplicationStyledSelect>
+
       <ApplicationStyledInput
-        value={value}
-        data-testid={TEST_IDS.fullNameInput}
-        placeholder="Địa chỉ chi tiết"
+        value={value.detail}
+        placeholder="Số nhà, tên đường, phường/xã"
         error={!!error}
-        sx={{ maxWidth: { xs: "100%", sm: 300, md: 300 } }}
+        required
+        sx={{ width: '100%', maxWidth: 400 }}
         onChange={(e) => {
-          setValue(e.target.value);
+          setValue(prev => ({ ...prev, detail: e.target.value }));
           setError("");
         }}
       />
